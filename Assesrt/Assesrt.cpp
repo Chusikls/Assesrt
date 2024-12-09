e92083050c38e4365162b89922b0aeb92fd33ba4
Chusikls <Chusikl@yandex.ru>
2024-12-09 11:54:42 +0300

    Добавьте файлы проекта.

---
+++ Assesrt/Assesrt.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <cassert>
+float travelTime(float distance, float speed) {
+        float travelTime = 0;
+        travelTime = distance / speed;
+        return travelTime;
+}
+
+int main()
+{
+    float distance, speed;
+    std::cout << "Enter distance: \n";
+    std::cin >> distance;
+    std::cout << "Enter speed: \n";
+    std::cin >> speed;
+    assert(speed > 0);
+    std::cout << "Time: " << travelTime(distance, speed);
+}
+
